0cefd99c6046c0e11d1deaad262dd919f94291d1
Julien Costa castro <julien.costa-castro@etu.umontpellier.fr>
2026-07-13 00:36:01 +0200

    perf: adjust Lenis lerp to 0.1 for snappier, more natural scroll feeling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,8 +147,7 @@ export default function Home() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.1,
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
